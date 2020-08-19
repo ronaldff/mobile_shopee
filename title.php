a8567bef61cdfb21b3ddb54029e45181a7422523
piyush shyam <piyush.d.shyam@gmail.com>
2020-08-19 22:48:51 +0530

    html title change according to the page

--- title.php
+++ title.php
@@ -3,21 +3,6 @@
   $path = $_SERVER['PHP_SELF'];
   $page = basename($path);
 
-  if(isset($_GET['id']) && !empty($_GET['id'])){
-    $cat_id = get_safe_value($conn,$_GET['id']);
-    $get_products = "SELECT * FROM product WHERE categories_id='{$cat_id}' AND status='1'";
-    $product_result = mysqli_query($conn, $get_products);
-    if(mysqli_num_rows($product_result) > 0){
-      $product_data = mysqli_fetch_assoc($product_result);      
-    }
-  }
-
-  if(isset($product_data )){
-    $category_name = ucfirst($product_data['category_name']);
-  } else {
-    $category_name = '';
-  }
-
   switch ($page) {
     case "index.php":
       $title = 'Home';
@@ -26,7 +11,17 @@
       $title = 'Contact';
       break;
     case "category_wise_product.php":
-      $title = $category_name;
+      if(isset($_GET['id']) && !empty($_GET['id'])){
+        $cat_id = get_safe_value($conn,$_GET['id']);
+        $get_products = "SELECT * FROM product WHERE categories_id='{$cat_id}' AND status='1'";
+        $product_result = mysqli_query($conn, $get_products);
+        if(mysqli_num_rows($product_result) > 0){
+          $product_data = mysqli_fetch_assoc($product_result);
+          $title = ucfirst($product_data['category_name']);      
+        }
+      } else {
+        $title = '';
+      }
       break;
     default:
       $title = "";
